b6be324953fe6ec7968019da83bddb1ccbdab4cc
Omer Tuchfeld <omer@tuchfeld.dev>
2023-06-12 11:41:05 +0200

    Move to https://github.com/omertuc/recert/

--- certgraph/src/cluster_crypto/distributed_cert.rs
+++
@@ -1,8 +0,0 @@
-use super::{certificate, locations::Locations};
-
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub(crate) struct DistributedCert {
-    pub(crate) certificate: certificate::Certificate,
-    pub(crate) locations: Locations,
-}
-
